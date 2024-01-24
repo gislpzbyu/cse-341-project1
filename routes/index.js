34919d2648380888bf46886d92f846cd94826065
gislpzbyu <gislpzbyu@byui.edu>
2024-01-23 22:56:28 -0600

    lastvideo

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Hello Word')});
+//router.get('/', (req, res) => {res.send(`Hello World`); });
+
+//router.post('/', (req, res) => {
+    //res.send('Post Request Ready');})//
+
+router.use('/users', require('./users'));
 
 module.exports = router;
