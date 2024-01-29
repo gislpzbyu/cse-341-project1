18984a9364c35d110199771aca7bc7cc26690e70
gislpzbyu <gislpzbyu@byui.edu>
2024-01-29 01:28:32 -0600

    contacts 2

--- controllers/users.js
+++ controllers/users.js
@@ -18,8 +18,53 @@ result.toArray().then((users) => {
     res.status(200).json(users[0]);
 });
 };
+
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id:userId }, user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('users').remove({ _id:userId }, true);
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
-
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
   const express = require('express');
-  
+  const bodyParser = require('body-parser');
   const mongodb = require('./data/database');
   const app = express();
 
   const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
 
 
 
-mongodb.initDb((err => {
+mongodb.initDb((err) => {
     if(err) {
         console.log(err);
     }
@@ -17,4 +18,4 @@ mongodb.initDb((err => {
 
         app.listen(port, () => {console.log(`Running on port ${port}`)});
     }
-}));
+});
